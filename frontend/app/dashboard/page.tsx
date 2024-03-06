0c68507e557c4b717ee1d49bcc3ee2ffef78f671
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2024-03-06 21:29:35 +0200

    feat: loading spinner while data is fetched

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -4,22 +4,34 @@ import LineChart from './_components/LineChart';
 import HeadingText from '@/components/heading-text';
 import StatusCard from './_components/StatusCard';
 import { useFetchData } from '@/services';
+import { ImSpinner2 } from 'react-icons/im';
 
 const Dashboard = () => {
-    const { data: chartData, error, loading } = useFetchData();
+    const { data, error, loading } = useFetchData();
 
-    if (loading) return <div>Loading...</div>;
-    if (error || !chartData) return <div>Error: {error}</div>;
-
-    return (
-        <div className="flex flex-col items-center">
-            <HeadingText subtext="">Dashboard</HeadingText>
-            <div className="w-full lg:w-1/2">
-                <LineChart data={chartData} />
+    if (loading)
+        return (
+            <div className="flex flex-col items-center justify-center w-full h-screen">
+                <ImSpinner2 className="animate-spin h-12 w-12" />
             </div>
-            <StatusCard data={chartData} />
-        </div>
-    );
+        );
+    else {
+        if (error || (!data && !loading))
+            return <div>Error: {error}</div>;
+        else {
+            return (
+                <div className="flex flex-col items-center">
+                    <HeadingText subtext="">
+                        Dashboard
+                    </HeadingText>
+                    <div className="w-full lg:w-1/2">
+                        <LineChart data={data} />
+                    </div>
+                    <StatusCard data={data} />
+                </div>
+            );
+        }
+    }
 };
 
 export default Dashboard;
