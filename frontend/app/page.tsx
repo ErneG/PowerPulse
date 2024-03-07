255fb315374c18685eb41b7d11cb017d1c02b693
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2024-03-07 15:13:52 +0200

    fix: remove feature cards

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import Hero from '@/components/pages/hero';
-import FeatureCards from '@/components/pages/feature-cards';
 
 export default function Home() {
     return (
         <main>
             <Hero />
-            <FeatureCards />
         </main>
     );
 }
