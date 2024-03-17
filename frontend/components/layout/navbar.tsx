bb03922869f42cc7f06e3479b1d16cc9e4f18a35
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2024-03-18 01:13:26 +0200

    feat: add sidebar

--- frontend/components/layout/navbar.tsx
+++ frontend/components/layout/navbar.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Link from 'next/link';
 import { ModeToggle } from '@/components/mode-toggle';
 import { navLinks } from '@/lib/links';
-import { settings } from '@/config';
+import { settings } from '@/config/settings';
 import { Button } from '../ui/button';
 
 export default function Navbar() {
@@ -13,14 +13,6 @@ export default function Navbar() {
     const handleClick = async () => {
         setNavbar(false);
     };
-
-    useEffect(() => {
-        if (navbar) {
-            document.body.style.overflow = 'hidden';
-        } else {
-            document.body.style.overflow = 'auto';
-        }
-    }, [navbar]);
 
     return (
         <header className="select-none">
@@ -32,6 +24,7 @@ export default function Navbar() {
                         </h1>
                     </Link>
                     <div className="flex gap-1 md:hidden">
+                        <ModeToggle />
                         <button
                             className="rounded-md p-2 text-primary outline-none focus:border focus:border-primary"
                             aria-label="Hamburger Menu"
@@ -64,27 +57,78 @@ export default function Navbar() {
                                 </svg>
                             )}
                         </button>
-                        <ModeToggle />
                     </div>
                 </div>
+
+                {/* Mobile navigation */}
                 <div
-                    className={`absolute left-0 top-0 z-10 justify-center rounded-md border bg-background md:static md:block md:border-none  ${
-                        navbar ? 'block' : 'hidden'
+                    className={`fixed top-0 right-0  h-full w-48 bg-background transform transition-transform duration-300 ease-in-out z-10 justify-center rounded-md border md:hidden  ${
+                        navbar
+                            ? 'translate-x-0'
+                            : 'translate-x-full'
                     }`}
-                    style={{ width: '100%', maxWidth: '20rem' }}>
-                    <ul className="flex flex-col items-center space-y-4 text-primary opacity-60 md:flex-row md:space-x-6 md:space-y-0">
+                    onClick={handleClick}
+                    style={{ maxWidth: '20rem' }}>
+                    <button
+                        className="rounded-md fixed top-2 right-4 p-2 text-primary outline-none focus:border focus:border-primary"
+                        aria-label="Hamburger Menu"
+                        onClick={() => setNavbar(!navbar)}>
+                        {navbar ? (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6 "
+                                viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path
+                                    fillRule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        ) : (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6 "
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                strokeWidth={2}>
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                            </svg>
+                        )}
+                    </button>
+                    <ul className="flex flex-col items-center space-y-4 text-primary opacity-60 mt-12">
                         {navLinks.map((link, index) => (
                             <Button
                                 key={index}
-                                asChild
                                 variant="ghost"
-                                className="w-full h-full">
+                                className="w-40">
                                 <Link
                                     href={link.path}
-                                    onClick={handleClick}>
+                                    onClick={handleClick}
+                                    className="w-full">
                                     {link.route}
                                 </Link>
                             </Button>
+                        ))}
+                    </ul>
+                </div>
+
+                {/* Desktop navigation */}
+                <div className="hidden md:block">
+                    <ul className="flex space-x-6 items-center justify-center">
+                        {navLinks.map((link, index) => (
+                            <li key={index}>
+                                <Link href={link.path}>
+                                    <p className="text-primary hover:text-secondary">
+                                        {link.route}
+                                    </p>
+                                </Link>
+                            </li>
                         ))}
                     </ul>
                 </div>
